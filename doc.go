53ca114b033915cbc4ebfda1034d0bfdc6249e5f
Prnyself <281239768@qq.com>
2021-04-30 15:07:43 +0800

    *: Rename package
    Signed-off-by: Prnyself <281239768@qq.com>

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 /*
-Package example provided support for local file system.
+Package ftp provided support for ftp by go-storage.
 */
-package example
+package ftp
 
 //go:generate go run -tags tools github.com/aos-dev/go-storage/v3/cmd/definitions service.toml
--- storage.go
+++ storage.go
@@ -1,4 +1,4 @@
-package example
+package ftp
 
 import (
 	"context"
