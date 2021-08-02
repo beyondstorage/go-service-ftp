f9195f902bd4186397ac1459c30feb8dbbc2e848
N/P硅 <npofsi@outlook.com>
2021-08-02 19:31:28 +0800

    update: Simplify test

--- tests/README.md
+++ tests/README.md
@@ -20,7 +20,6 @@ Set following environment variables:
 
 ```shell
 export STORAGE_FTP_INTEGRATION_TEST=on
-export STORAGE_FTP_NAME=name:port
 ```
 
 Run tests
--- tests/utils_test.go
+++ tests/utils_test.go
@@ -1,21 +1,16 @@
 package tests
 
 import (
-	"os"
 	"testing"
 
-	_ "github.com/beyondstorage/go-service-ftp"
-	"github.com/beyondstorage/go-storage/v4/services"
+	ftp "github.com/beyondstorage/go-service-ftp"
 	"github.com/beyondstorage/go-storage/v4/types"
-	"github.com/google/uuid"
 )
 
 func initTest(t *testing.T) (store types.Storager) {
 	t.Log("Setup test for ftp")
 
-	hostName := os.Getenv("STORAGE_FTP_NAME")
-
-	store, err := services.NewStoragerFromString(("ftp://") + hostName + "/" + uuid.New().String())
+	store, err := ftp.NewStorager()
 	if err != nil {
 		t.Errorf("create storager: %v", err)
 	}
